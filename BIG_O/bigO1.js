3e9159f87bf9484d12f475888751f608dc785b7b
Md-Rohed <mdrohed85@gmail.com>
2022-02-16 14:09:28 +0600

    Big O(1) && O(n)

---
+++ BIG_O/bigO1.js
@@ -0,0 +1,8 @@
+const boxes = [0, 1, 2, 3, 4, 5];
+function logFirstbox(boxes) {
+  console.log(boxes[0]); //O(1)
+  console.log(boxes[1]); //O(1)
+}
+
+//so big(O) of this function is O(1)
+// Constant No Loops
---
+++ BIG_O/bigOn.js
@@ -0,0 +1,14 @@
+function fun(input) {
+  let a = 10; //O(1)
+  a = 50 + 4; //O(n)
+  for (let i = 0; i < inout.length; i++) {
+    //O(n)
+    anotherfunction(); //O(n)
+    let stranger = true; //O(1)
+    a++; //O(n)
+  }
+  return a; //O(1)
+}
+//Big (O)== (3+4n)
+// final Big (O)=O(n)
+//O(n) Linear- for loops, while loops through n items
